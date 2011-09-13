271e4d43672fcafeddbcbbd8b84807b2c2d76c36
Aleksander Pohl <apohllo@o2.pl>
2011-09-13 19:03:09 +0200

    - Concept hierarchy support

--- lib/rlp/semantics/concept.rb
+++ lib/rlp/semantics/concept.rb
@@ -10,12 +10,28 @@ module Rlp
     # the Polish language. Since one concept might be expressend in many
     # ways, one concept has many spellings.
     class Concept < Model
+      # The descriptive name of the concept.
       field :name, :string, :index => :hash
-      field :ssjp_id, :integer
 
+      # The SSJP DB id of the concept.
+      field :ssjp_id, :integer, :index => :hash
+
+      # Spellings, i.e. text representations of the concept, split into
+      # segments which are mapped to RLP flexemes.
       has_many :spellings
 
-      validates_presence_of :spellings, :name
+      # Direct children of the concept - its specialisations.
+      has_many :children, :class_name => "Rlp::Semantics::Concept"
+
+      # Direct parents of the concept - its generalisations.
+      has_many :parents, :class_name => "Rlp::Semantics::Concept"
+
+      validates_presence_of :name, :ssjp_id
+
+      # String representation of the concept.
+      def to_s
+        self.name
+      end
     end
   end
 end
