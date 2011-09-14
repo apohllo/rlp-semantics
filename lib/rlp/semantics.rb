fde18b4aba3e6c79f62739f50b3216033ab14a5e
Aleksander Pohl <apohllo@o2.pl>
2011-09-14 14:36:40 +0200

    - Add category to master file

--- lib/rlp/semantics.rb
+++ lib/rlp/semantics.rb
@@ -1,5 +1,7 @@
 require 'rlp/semantics/database'
 require 'rlp/semantics/model'
+
+require 'rlp/semantics/category'
 require 'rlp/semantics/concept'
 require 'rlp/semantics/spelling'
 require 'rlp/semantics/segment'
